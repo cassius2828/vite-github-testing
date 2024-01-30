97526685544e4ce7e0f45019ad4c2890218e4f58
Cassius Reynolds <cassius2828@gmail.com>
2024-01-29 17:01:46 -0800

    updating files

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     // the name of the repo with forward slash
-    base: "/vite-github-testing/"
+    base: "/vite-github/"
 })
